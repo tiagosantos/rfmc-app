46e7ac6088135310553dc346208a1f8b9062042b
Carlos Tiago <ontiago@gmail.com>
2015-05-05 15:03:10 -0300

    Adaptador para heroku com postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150505174307) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "businesses", force: :cascade do |t|
     t.string   "corporate_name"
     t.string   "nickname"
